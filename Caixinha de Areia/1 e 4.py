e4ffead2b70edacf31ea6a0f29ea929050901997
OtavioFinger <otaviofinger302@gmail.com>
2025-11-02 11:27:52 -0300

    1 e 4

---
+++ Caixinha de Areia/1 e 4.py
@@ -0,0 +1,10 @@
+# Média de notas: Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+
+nota1 = int(input("Leia a primeira nota: "))
+nota2 = int(input("Leia a segunda nota: "))
+nota3 = int(input("Leia a terceira nota: "))
+nota4 = int(input("Leia a quarta nota: "))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print(f"Nota média: {media}")
