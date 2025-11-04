342213c154232580a9ce5e7b509d10b64571ff92
OtavioFinger <otaviofinger302@gmail.com>
2025-11-04 10:02:26 -0300

    1 das 9 e 10

---
+++ Caixinha de Areia/1 e 10.py
@@ -0,0 +1,7 @@
+# Celsius para Fahrenheit: 
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+
+celsius = float(input("Digite um valor em Cº: "))
+
+fahrenheit =  celsius * 1.8 + 32 
+print(f"Fahrenheit: {fahrenheit}")
---
+++ Caixinha de Areia/1 e 9.py
@@ -0,0 +1,6 @@
+# Fahrenheit para Celsius: 
+# Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius. 
+
+fahrenheit = float(input("Digite um valor em fahrenheit: "))
+celsisu =  5 / 9 * (fahrenheit - 32)
+print(f"Celsius: {celsisu:.2f}")
