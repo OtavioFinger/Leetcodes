c7573dadb18f6d58e83a8ce324bb9996eb7bbfd9
OtavioFinger <otaviofinger302@gmail.com>
2025-11-18 13:02:17 -0300

    2 e 22

---
+++ Caixinha de Areia/2 e 22.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça um número inteiro e determine se ele é par ou ímpar.
+
+numero = int(input("Digite um número: "))
+
+if numero % 2 == 0:
+    print("É par!")
+else:
+    print("É ímpar!")
